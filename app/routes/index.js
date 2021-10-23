92f305ddca19e7fca6f86350150d2fb673e50ce1
Rodrigo Valdés <rodrigovaldes@gmail.com>
2021-10-23 03:17:44 -0300

    Update de aplicación.

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  //res.render('index', { title: 'Express' });
-  res.send('<h1>Express NodeJS Web Framework</h1>')
+  res.render('index', { title: 'NodeJS Express Web Framework' });
+  //res.send('<h1>Express NodeJS Web Framework</h1>')
 });
 
 module.exports = router;
